c2af74800b5a75b115c24b94591a7ebc2f44e960
Chris Boakes <chris@chrisboakes.com>
2020-03-11 11:34:10 +0000

    Header/nav structure

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Nav from '../../elements/nav/Nav.svelte';
+	import Logo from '../../elements/logo/Logo.svelte';
+
+	export let segment;
+</script>
+
+<style>
+	header {
+		
+	}
+</style>
+
+<header>
+	<Logo />
+	<Nav {segment}/>
+</header>
