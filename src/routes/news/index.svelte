8513c23c253b070f46d928efab15ed26a4a5eec5
Chris Boakes <chris@chrisboakes.com>
2020-03-13 14:41:22 +0000

    Make a call for dynamic data client-side

--- src/routes/news/index.svelte
+++ src/routes/news/index.svelte
@@ -1,16 +1,18 @@
-<script context="module">
-	export async function preload({ params, query }) {
-		const res = await this.fetch('https://my-json-server.typicode.com/chrisboakes/svelte-sapper-demo/news-articles')
+<script>
+	let posts = [];
+
+	async function getLists() {
+		const res = await fetch('https://my-json-server.typicode.com/chrisboakes/svelte-sapper-demo/news-articles')
 		const data = await res.json();
 
 		return {
 			posts: data
 		}
 	}
-</script>
-
-<script>
-	export let posts;
+	
+	getLists().then((results) => {
+		posts = results.posts;
+	});
 </script>
 
 <svelte:head>
@@ -20,11 +22,9 @@
 <h1>News</h1>
 
 <ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='{post.section}/{post.slug}'>{post.title}</a></li>
-	{/each}
+	{#if posts}
+		{#each posts as post}
+			<li><a href='{post.section}/{post.slug}'>{post.title}</a></li>
+		{/each}
+	{/if}
 </ul>
