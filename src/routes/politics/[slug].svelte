4cc40f2aa6abbef89334ce375a6bf80d874a9d8e
Chris Boakes <chris@chrisboakes.com>
2020-03-13 16:00:15 +0000

    Politics section SSR'd

---
+++ src/routes/politics/[slug].svelte
@@ -0,0 +1,29 @@
+<script context="module">
+	export async function preload({ params, query }) {
+		// the `slug` parameter is available because
+		// this file is called [slug].svelte
+		const res = await this.fetch(`https://my-json-server.typicode.com/chrisboakes/svelte-sapper-demo/${params.slug}`);
+		const data = await res.json();
+
+		if (res.status === 200) {
+			return { post: data };
+		} else {
+			this.error(res.status, data.message);
+		}
+	}
+</script>
+
+<script>
+	export let post;
+</script>
+
+<svelte:head>
+	<title>{post.title}</title>
+</svelte:head>
+
+<h1>{post.title}</h1>
+<p>{post.standfirst}</p>
+
+<div class='content'>
+	{@html post.content}
+</div>
