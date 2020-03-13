75e0bd8dfb114dfc9ca7ecf26a0eaf01f4715fe8
Chris Boakes <chris@chrisboakes.com>
2020-03-13 15:50:49 +0000

    SSR politics articles

--- src/routes/politics.svelte
+++ src/routes/politics.svelte
@@ -1,5 +1,30 @@
+<script context="module">
+	export async function preload({ params, query }) {
+		const res = await this.fetch('https://my-json-server.typicode.com/chrisboakes/svelte-sapper-demo/politics-articles')
+		const data = await res.json();
+
+		return {
+			posts: data
+		}
+	}
+</script>
+
+<script>
+	export let posts;
+</script>
+
 <svelte:head>
 	<title>Politics</title>
 </svelte:head>
 
 <h1>Politics</h1>
+
+<ul>
+	{#each posts as post}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a rel='prefetch' href='{post.section}/{post.slug}'>{post.title}</a></li>
+	{/each}
+</ul>
